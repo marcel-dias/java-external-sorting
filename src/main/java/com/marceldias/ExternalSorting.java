cf1138100f1f08fb0bce870ba4adaa8a2c14860b
Marcel Dias <marceldiass@gmail.com>
2016-06-14 23:05:32 -0300

    Initial commit

---
+++ src/main/java/com/marceldias/ExternalSorting.java
@@ -0,0 +1,11 @@
+package com.marceldias;
+
+public class ExternalSorting {
+
+    public static void main(String... args) {
+        System.out.println(" Running External Sorting!");
+
+
+        System.out.println(" External Sorting successfully executed!");
+    }
+}
