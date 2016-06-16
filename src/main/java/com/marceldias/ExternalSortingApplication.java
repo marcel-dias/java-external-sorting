cf4ec89e4b8d231aaa3e1c38e9339e0a4879175c
Marcel Dias <marceldiass@gmail.com>
2016-06-16 03:46:35 -0300

    update Application with File reader and splitter logic

--- src/main/java/com/marceldias/ExternalSortingApplication.java
+++ src/main/java/com/marceldias/ExternalSortingApplication.java
@@ -1,10 +1,29 @@
 package com.marceldias;
 
+import com.marceldias.externalsorting.FileSplitter;
+
+import java.io.File;
+import java.util.Map;
+
 public class ExternalSortingApplication {
 
     public static void main(String... args) {
         System.out.println(" Running External Sorting!");
 
+        //validate ExternalSortingProperties.FILENAME.value();
+
+        FileSplitter fileSplitter = new FileSplitter();
+        Map<String, File> tempFiles = fileSplitter.split();
+
+//      sort in alphabetic order (filelist);
+//      for each file in filelist
+
+        // asynchronous
+        // read, sort in alphabetic order and override file
+        // read and merge
+
+        //synchronous
+        // maybe read, sort and merge
 
         System.out.println(" External Sorting successfully executed!");
     }
