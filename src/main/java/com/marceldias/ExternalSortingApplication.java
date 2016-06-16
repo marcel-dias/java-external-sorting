203fb76a56a83860602bdbb858a352b146cb63bd
Marcel Dias <marceldiass@gmail.com>
2016-06-15 23:22:08 -0300

    Rename executable class to ExternalSortingApplication

--- src/main/java/com/marceldias/ExternalSortingApplication.java
+++ src/main/java/com/marceldias/ExternalSortingApplication.java
@@ -1,6 +1,6 @@
 package com.marceldias;
 
-public class ExternalSorting {
+public class ExternalSortingApplication {
 
     public static void main(String... args) {
         System.out.println(" Running External Sorting!");
