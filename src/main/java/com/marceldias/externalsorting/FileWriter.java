d709e61ef678d1b91db98e04726d667ed6adea92
Marcel Dias <marceldiass@gmail.com>
2016-06-20 02:22:28 -0300

    Add methods to write multiple lines and override file

--- src/main/java/com/marceldias/externalsorting/FileWriter.java
+++ src/main/java/com/marceldias/externalsorting/FileWriter.java
@@ -1,18 +1,57 @@
 package com.marceldias.externalsorting;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
 public class FileWriter {
 
     private static final Boolean append = Boolean.TRUE;
 
+    public static void writeLine(String line, File file, Boolean append) {
+        writeLines(Arrays.asList(line), file, append);
+    }
+
     public static void appendLine(String line, File file) {
+        writeLine(line, file, append);
+    }
+
+    public static void writeLines(List<String> lines, File file, Boolean append) {
         try (BufferedWriter bw = new BufferedWriter(new java.io.FileWriter(file, append))) {
-            bw.write(line);
-            bw.newLine();
+            for (String line : lines) {
+                bw.write(line);
+                bw.newLine();
+            }
             bw.flush();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    public static void mergeFiles(List<File> files) {
+        File output = Paths.get(ExternalSortingProperties.OUTPUT.value()).toFile();
+        try (BufferedWriter bw = new BufferedWriter(new java.io.FileWriter(output, false))) {
+            for (File file : files) {
+                try (BufferedReader br = Files.newBufferedReader(file.toPath(), StandardCharsets.UTF_8)) {
+                    String line;
+                    while ((line = br.readLine()) != null ) {
+                        bw.write(line);
+                        bw.newLine();
+                    }
+                } catch (NoSuchFileException e) {
+                    continue;
+                } catch (Exception e) {
+                    continue;
+                }
+                bw.flush();
+            }
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
