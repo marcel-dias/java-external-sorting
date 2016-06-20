3cdd6000924b1f3b60ddd526433d194bd436f023
Marcel Dias <marceldiass@gmail.com>
2016-06-19 23:42:18 -0300

    Change from take() to pool with 500 ms of timeout

--- src/main/java/com/marceldias/externalsorting/FileSplitterWriter.java
+++ src/main/java/com/marceldias/externalsorting/FileSplitterWriter.java
@@ -3,6 +3,7 @@ package com.marceldias.externalsorting;
 import java.io.File;
 import java.nio.file.Paths;
 import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
 
 public class FileSplitterWriter extends FileWriter implements Callable<Boolean> {
     private FileSplitter fileSplitter;
@@ -16,9 +17,11 @@ public class FileSplitterWriter extends FileWriter implements Callable<Boolean> 
     public Boolean call() {
         try {
             while (!fileSplitter.isReaderDone() || (fileSplitter.isReaderDone() && !fileSplitter.getLinesQueue().isEmpty())) {
-                String lineToProcess = fileSplitter.getLinesQueue().take();
-                proccessLine(lineToProcess);
-                fileSplitter.increment();
+                String lineToProcess = fileSplitter.getLinesQueue().poll(500l, TimeUnit.MILLISECONDS);
+                if (lineToProcess != null) {
+                    proccessLine(lineToProcess);
+                    fileSplitter.increment();
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
