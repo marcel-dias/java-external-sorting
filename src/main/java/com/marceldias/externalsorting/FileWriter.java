705707019f517554d148a0244ce9c42b1157029d
Marcel Dias <marceldiass@gmail.com>
2016-06-19 21:21:51 -0300

    Move appendLine method to static

--- src/main/java/com/marceldias/externalsorting/FileWriter.java
+++ src/main/java/com/marceldias/externalsorting/FileWriter.java
@@ -6,9 +6,9 @@ import java.io.IOException;
 
 public class FileWriter {
 
-    private final Boolean append = Boolean.TRUE;
+    private static final Boolean append = Boolean.TRUE;
 
-    public void appendLine(String line, File file) {
+    public static void appendLine(String line, File file) {
         try (BufferedWriter bw = new BufferedWriter(new java.io.FileWriter(file, append))) {
             bw.write(line);
             bw.newLine();
