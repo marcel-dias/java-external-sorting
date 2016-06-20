511377afca3a73cf78d3713822f2d02be2315701
Marcel Dias <marceldiass@gmail.com>
2016-06-20 00:43:57 -0300

    Cleanup: remove unused code

--- src/main/java/com/marceldias/externalsorting/exception/ExternalSortingException.java
+++ src/main/java/com/marceldias/externalsorting/exception/ExternalSortingException.java
@@ -5,8 +5,4 @@ public class ExternalSortingException extends RuntimeException {
     public ExternalSortingException(String message, Throwable cause) {
         super(message, cause);
     }
-
-    public ExternalSortingException(String message) {
-        super(message);
-    }
 }
