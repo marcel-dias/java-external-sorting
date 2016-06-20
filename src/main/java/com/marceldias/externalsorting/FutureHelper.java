6943c7100c56dc28727439563930f963f3e1718a
Marcel Dias <marceldiass@gmail.com>
2016-06-20 02:23:27 -0300

    Add FutureHelper to handle Future executions

---
+++ src/main/java/com/marceldias/externalsorting/FutureHelper.java
@@ -0,0 +1,29 @@
+package com.marceldias.externalsorting;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public final class FutureHelper {
+
+    public static void waitExecution(Future... futures) {
+        for (Future f : futures) {
+            waitExecution(f);
+        }
+    }
+
+    public static <T> T waitExecution(Future<T> future) {
+        try {
+            return future.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void waitExecution(List<Future> futures) {
+        waitExecution(futures.toArray(new Future[futures.size()]));
+    }
+}
