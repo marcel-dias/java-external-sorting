b327dcf96af858008b0ae443f4ff5e073743a652
Marcel Dias <marceldiass@gmail.com>
2016-06-26 09:50:45 -0300

    Organize imports

--- src/main/java/com/marceldias/externalsorting/FileWriter.java
+++ src/main/java/com/marceldias/externalsorting/FileWriter.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.Arrays;
