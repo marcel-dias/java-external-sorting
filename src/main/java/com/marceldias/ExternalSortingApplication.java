9ea60dcd96e8d88372f5da1ad001cefb3c7e9282
Marcel Dias <marceldiass@gmail.com>
2016-06-27 02:38:59 -0300

    Update to use new way to handle splitting, sorting, k-way merge

--- src/main/java/com/marceldias/ExternalSortingApplication.java
+++ src/main/java/com/marceldias/ExternalSortingApplication.java
@@ -1,19 +1,30 @@
 package com.marceldias;
 
+import com.marceldias.externalsorting.AlphabeticalOrderValidator;
+import com.marceldias.externalsorting.ExternalSortingProperties;
+import com.marceldias.externalsorting.FileMerger;
 import com.marceldias.externalsorting.FileSorter;
 import com.marceldias.externalsorting.FileSplitter;
 import com.marceldias.externalsorting.FileWriter;
+import com.marceldias.externalsorting.Sorter;
 import com.marceldias.externalsorting.TimeMetric;
 
 import java.io.File;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ExternalSortingApplication {
 
     public static void main(String... args) {
-        new ExternalSortingApplication().execute();
+
+        if (args.length > 0 && args[0].equals("validate")) {
+            new AlphabeticalOrderValidator().validate(ExternalSortingProperties.OUTPUT.value());
+        } else {
+            new ExternalSortingApplication().execute();
+        }
     }
 
     private void execute() {
@@ -23,24 +34,24 @@ public class ExternalSortingApplication {
         FileSplitter fileSplitter = new FileSplitter();
         Map<String, File> tempFiles = fileSplitter.split();
 
-        // asynchronous
-        // read, sort in alphabetic order and override file
-        List<String> orderedFiles = FileSorter.sort(tempFiles);
+        // multithread read, sort in alphabetic order and override file
+        FileSorter fileSorter = new FileSorter();
+        fileSorter.sort(new HashSet<>(tempFiles.values()));
+
+        Sorter sorter = new Sorter();
+        List<String> orderedFiles = sorter.sort(new LinkedList<>(tempFiles.keySet()));
+        orderedFiles = new FileMerger().doMerge(orderedFiles, fileSplitter.getSameFirstCharFilename());
 
         // read and merge
         List<File> files = getOrderedFileList(orderedFiles, tempFiles);
-        FileWriter.mergeFiles(files);
+        FileWriter fileWriter = new FileWriter();
+        fileWriter.mergeFiles(files);
 
         timeMetric.print();
         System.out.println(" External Sorting successfully executed!");
-        System.exit(0);
     }
 
     protected List<File> getOrderedFileList(List<String> filenames, Map<String, File> tempFiles) {
-        List<File> orderedFiles = new LinkedList<>();
-        for (String filename : filenames) {
-            orderedFiles.add(tempFiles.get(filename));
-        }
-        return orderedFiles;
+        return filenames.stream().map(tempFiles::get).collect(Collectors.toCollection(() -> new LinkedList<>()));
     }
 }
