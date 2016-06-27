5f9bf569008c18cc6b9584464650670b57a3c3d8
Marcel Dias <marceldiass@gmail.com>
2016-06-27 00:15:19 -0300

    Move getFile back to keep it thread safe

--- src/main/java/com/marceldias/externalsorting/FileSplitterWriter.java
+++ src/main/java/com/marceldias/externalsorting/FileSplitterWriter.java
@@ -1,11 +1,13 @@
 package com.marceldias.externalsorting;
 
 import java.io.File;
+import java.nio.file.Paths;
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
 public class FileSplitterWriter extends FileWriter implements Callable<Boolean> {
     private FileSplitter fileSplitter;
+    private String tempFilesDir = ExternalSortingProperties.TEMP_FILES_DIR.value();
 
     public FileSplitterWriter(FileSplitter fileSplitter) {
         this.fileSplitter = fileSplitter;
@@ -15,21 +17,44 @@ public class FileSplitterWriter extends FileWriter implements Callable<Boolean> 
     public Boolean call() {
         try {
             while (!fileSplitter.isReaderDone() || (fileSplitter.isReaderDone() && !fileSplitter.getLinesQueue().isEmpty())) {
-                String lineToProcess = fileSplitter.getLinesQueue().poll(500l, TimeUnit.MILLISECONDS);
+                String lineToProcess = fileSplitter.getLinesQueue().poll(500L, TimeUnit.MILLISECONDS);
                 if (lineToProcess != null) {
                     proccessLine(lineToProcess);
                     fileSplitter.increment();
                 }
             }
         } catch (Exception e) {
-            e.printStackTrace();
             return Boolean.FALSE;
         }
         return Boolean.TRUE;
     }
 
     protected void proccessLine(String line) {
-        File file = fileSplitter.getFile(line);
+        File file = getFile(line);
         appendLine(line, file);
+    }
+
+    protected File getFile( String line) {
+        return getFile("", line);
+    }
+
+    protected File getFile(String prefix, String line) {
+        char start = line.charAt(0);
+        String filename = (prefix + start).toLowerCase();
+
+        File file = fileSplitter.getTempFiles().get(filename);
+        if (file == null) {
+            file = Paths.get(tempFilesDir, filename + ".txt").toFile();
+            file.deleteOnExit();
+            fileSplitter.addTempFile(filename, file);
+        } else {
+            Long maxTempFileSize = Long.valueOf(ExternalSortingProperties.MAX_TEMP_FILE_SIZE.value());
+            if (fileSplitter.isFileExhausted(filename) || file.length() >= maxTempFileSize) {
+                fileSplitter.addExhaustedFile(filename);
+                file = getFile(filename, line.substring(1));
+            }
+        }
+        fileSplitter.checkSameFirstCharFilename(file);
+        return file;
     }
 }
--- src/test/java/com/marceldias/externalsorting/FileSplitterWriterTest.java
+++ src/test/java/com/marceldias/externalsorting/FileSplitterWriterTest.java
@@ -7,6 +7,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -16,6 +17,8 @@ public class FileSplitterWriterTest {
 
     private FileSplitterWriter fileSplitterWriter;
     private FileSplitter fileSplitter;
+    private String tempFilesDir = ExternalSortingProperties.TEMP_FILES_DIR.value();
+    private String testFilename = "test.txt";
 
     @Before
     public void setUp() {
@@ -25,7 +28,7 @@ public class FileSplitterWriterTest {
 
     @After
     public void tearDown() throws IOException {
-        String[] testFiles = {"a.txt", "ab.txt"};
+        String[] testFiles = {"a.txt", "ab.txt", "abc.txt", "abcd.txt", testFilename};
         for (String file : testFiles) {
             Files.deleteIfExists(Paths.get(ExternalSortingProperties.TEMP_FILES_DIR.value(), file));
         }
@@ -56,4 +59,42 @@ public class FileSplitterWriterTest {
         Assert.assertThat(zFile.length(), Is.is(50l));
     }
 
+    @Test
+    public void testGetFile() {
+        String line = "abcdefg";
+        File file = fileSplitterWriter.getFile(line);
+        Assert.assertThat(file.getName(), IsEqual.equalTo("a.txt"));
+    }
+
+    @Test
+    public void testGetFileWithPrefixAndHugeSize() {
+        String fileContent = "abcdefghijklmnoprstuvxyz abcdefghijklmnoprstuvxyz abc\n" +
+                "abc defghi jklmnopr stuvxyz abcdefghijklmnoprstuvxyz abc\n" +
+                "abc zyxut defghijklmnoprstuvxyz abcdefghijklmnoprstuvxyz";
+        writeFile(testFilename, fileContent);
+        String filename = tempFilesDir + "/" + testFilename;
+        System.setProperty(ExternalSortingProperties.FILENAME.getLabel(), filename);
+        System.setProperty(ExternalSortingProperties.MAX_TEMP_FILE_SIZE.getLabel(), "50");
+
+        String line = "abcdefghijklmnoprstuvxyz abcdefghijklmnoprstuvxyz";
+        fileSplitter.split();
+        File file = fileSplitterWriter.getFile(line);
+
+        Assert.assertThat(file.getName(), IsEqual.equalTo("abcd.txt"));
+    }
+
+    private File writeFile(String filename, String fileContent) {
+        String tempFilesDir = ExternalSortingProperties.TEMP_FILES_DIR.value();
+        File file = Paths.get(tempFilesDir, filename).toFile();
+        file.deleteOnExit();
+        try (BufferedWriter bw = new BufferedWriter(new java.io.FileWriter(file, false))) {
+
+            bw.write(fileContent);
+            bw.newLine();
+            bw.flush();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        return file;
+    }
 }
